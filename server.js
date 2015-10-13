206992abb1f6ea3787f2229c247c15dd97be2ca7
Ian Zhang <ianzhang1986@gmail.com>
2015-10-13 13:55:40 -0700

    Reconfig configuration file

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
+// load config
+var config = require('.' + 'config.js');
 var express = require('express');
+var app = express();
+
+app.listen(3000);
+
+app.get("/", function(req, res) {
+  res.send(200);
+});
